ef6e895596ffbca2f49a2d82a734889d116d49a9
Sumit <sumitnarayan9091@gmail.com>
2024-09-20 10:22:56 +0530

    login route

--- models/User.js
+++ models/User.js
@@ -35,8 +35,8 @@ UserSchema.pre("save", async function () {
   this.password = await bcrypt.hash(this.password, salt);
 });
 
-UserSchema.method.comparePassword = async function (candidatePassword) {
-  const isMatch = await bcrypt.compare(candidatePassword, this.password);
+UserSchema.methods.comparePassword = async function (canditatePassword) {
+  const isMatch = await bcrypt.compare(canditatePassword, this.password);
   return isMatch;
 };
 
