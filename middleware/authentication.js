4b75643566a0882de42feeed0f4fe95888692adc
Sumit <sumitnarayan9091@gmail.com>
2024-09-22 03:34:07 +0530

    authenticate user

---
+++ middleware/authentication.js
@@ -0,0 +1,18 @@
+const CustomError = require("../errors");
+const { isTokenValid } = require("../utils");
+
+const authenticateUser = async (req, res, next) => {
+  const token = req.signedCookies.token;
+  if (!token) {
+    throw new CustomError.UnauthenticatedError("Authentication invalid");
+  }
+  try {
+    const { name, userId, role } = isTokenValid({ token });
+    req.user = { name, userId, role };
+    next();
+  } catch {
+    throw new CustomError.UnauthenticatedError("Authentication invalid");
+  }
+};
+
+module.exports = { authenticateUser };
