c80c13f529e19f63cfaba818382cc12de4bf8bda
Sumit <sumitnarayan9091@gmail.com>
2024-10-02 04:40:34 +0530

    createUserToken function

--- utils/index.js
+++ utils/index.js
@@ -1,3 +1,9 @@
-const { createJWT, isTokenValid,attachCookiesToResponse } = require("./jwt");
+const { createJWT, isTokenValid, attachCookiesToResponse } = require("./jwt");
+const createUserToken = require("./createTokenUser");
 
-module.exports = { createJWT, isTokenValid, attachCookiesToResponse };
+module.exports = {
+  createJWT,
+  isTokenValid,
+  attachCookiesToResponse,
+  createUserToken,
+};
