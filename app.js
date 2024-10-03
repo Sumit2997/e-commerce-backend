7ade1209c91324d6b750b4ea69aba45b08b2dff0
Sumit <sumitnarayan9091@gmail.com>
2024-10-03 05:39:07 +0530

    uploadImage Controller

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const connectDB = require("./db/connect");
 //rest of the package
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
+const fileUpload = require('express-fileupload')
 //routers
 const authRouter = require("./routes/authRoutes");
 const userRouter = require("./routes/userRoutes");
@@ -19,6 +20,8 @@ const errorHandlerMiddleware = require("./middleware/error-handler");
 app.use(morgan("tiny"));
 app.use(express.json());
 app.use(cookieParser(process.env.JWT_SECRET));
+app.use(express.static("./public"));
+app.use(fileUpload());
 
 app.get("/", (req, res) => {
   res.send("e-commerse api");
